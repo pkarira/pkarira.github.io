4e1398249c9289a3ac100ba8d1c8a9d0354e21b2
pkarira <pulkit5208@gmail.com>
2019-04-01 15:24:13 +0530

    completed background translation

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,36 @@
 var canvas = document.getElementById("myCanvas");
-/*canvas.setAttribute('width', window.innerWidth);
-canvas.setAttribute('height', window.innerHeight);*/
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
+ctx.beginPath();
+var windowWidth=window.innerWidth,windowHeight=window.innerHeight,circleRadius=20,circleX=circleRadius,circleY=windowHeight-circleRadius;
+var accelerationX=5,accelerationY=2,accelerationRadius=3,times=0;
+var draw=setInterval(redraw,5);
+function redraw()
+{
+ctx.clearRect(0, 0, canvas.width,canvas.height);
+ctx.beginPath();
+ctx.arc(circleX,circleY, circleRadius, 0, Math.PI * 2, true);
+ctx.fillStyle = "#ffffff";
+ctx.fill();
+circleX+=accelerationX;
+circleY-=accelerationY;
+circleRadius+=accelerationRadius;
+times++;
+if(times>250)
+{
+clearInterval(draw);
+ctx.clearRect(0, 0, canvas.width,canvas.height);
+ctx.beginPath();
+ctx.arc(windowWidth,windowHeight/2, windowWidth/2, 0, Math.PI * 2, true);
+ctx.fillStyle = "#ffffff";
+ctx.fill();}
+}
+function combiningTranslatingCircles()
+{
+
+}
+function translatingCircles()
+{
+
+}
