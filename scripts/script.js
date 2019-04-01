4f9176a9ea87232ac515edceb4acb8230dae17f7
pkarira <pulkit5208@gmail.com>
2019-04-01 18:43:27 +0530

    added smaller circles

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,13 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 var windowWidth=window.innerWidth,windowHeight=window.innerHeight,circleRadius=20,circleX=circleRadius,circleY=windowHeight-circleRadius;
-var accelerationX=5,accelerationY=2,accelerationRadius=3,times=0;
-var draw=setInterval(redraw,5);
-function redraw()
+var accelerationX=5,accelerationY=2,accelerationRadius=3,times=0,timesForSmallerCircles=0;
+var draw=setInterval(redrawBackground,5);
+var accelerationsX = [0,-0.5,-1,0.5,1];
+var accelerationsY = [2,1.3,1.3,1.3,1.3];
+var positionsY = [windowHeight,windowHeight,windowHeight,windowHeight,windowHeight];
+var positionsX = [windowWidth/2,windowWidth/2,windowWidth/2,windowWidth/2,windowWidth/2];
+function redrawBackground()
 {
 ctx.clearRect(0, 0, canvas.width,canvas.height);
 ctx.beginPath();
@@ -20,17 +24,39 @@ times++;
 if(times>250)
 {
 clearInterval(draw);
+smallerCircles = setInterval(translatingCircles,10);
+}
+}
+function drawCircle(X,Y,circleRadius,fillStyle)
+{
+ctx.beginPath();
+ctx.arc(X,Y, circleRadius, 0, Math.PI * 2, true);
+ctx.fillStyle = fillStyle;
+ctx.fill();
+}
+function translatingCircles()
+{
 ctx.clearRect(0, 0, canvas.width,canvas.height);
 ctx.beginPath();
 ctx.arc(windowWidth,windowHeight/2, windowWidth/2, 0, Math.PI * 2, true);
 ctx.fillStyle = "#ffffff";
-ctx.fill();}
-}
-function combiningTranslatingCircles()
+ctx.fill();
+for (i =0 ; i<5 ; i++)
 {
-
+var fillStyle;
+if(i==0)
+fillStyle="#ffff00"
+else
+if(i==1 || i==2)
+fillStyle="#ffffff"
+else
+if(i==3 || i==4)
+fillStyle="#000000"
+drawCircle(positionsX[i],positionsY[i],30,fillStyle)
+positionsX[i]+=accelerationsX[i];
+positionsY[i]-=accelerationsY[i];
 }
-function translatingCircles()
-{
-
+timesForSmallerCircles++;
+if(timesForSmallerCircles>200)
+clearInterval(smallerCircles);
 }
