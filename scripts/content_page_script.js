0ee994b9204548e63c4e4aeea5d42aee017bd56a
pkarira <pulkit5208@gmail.com>
2019-04-11 20:41:21 +0530

    added content link

--- scripts/content_page_script.js
+++ scripts/content_page_script.js
@@ -1,9 +1,30 @@
 var coll = document.getElementsByClassName("collapsible");
 var container = document.getElementById("list-container");
 var i,j,k;
-var a= ["a","b","c"];
-var b= [[["title1","link1"],["title2","link2"]],[["title3","link3"]],[["title3","link3"]]];
-for (i=0;i<3;i++)
+a=["Git","Javascript","Internet"];
+var links = {
+  "Git":[["how hash works","https://youtu.be/P6jD966jzlk"],
+  		["cherry-pick problems(no merge conflict)","https://devblogs.microsoft.com/oldnewthing/?p=98225 "],
+  		["how git merge works","https://stackoverflow.com/questions/14961255/how-does-git-merge-work-in-details"],
+  		["how git merge works"," https://www.quora.com/How-does-Git-merge-work"],
+  		["merge recursive strategy","http://blog.plasticscm.com/2011/09/merge-recursive-strategy.html"],
+  		["git internals","https://github.com/pluralsight/git-internals-pdf"],
+  		["git-scm","https://git-scm.com/book/en/v2"],
+  		["git insights","http://ftp.newartisans.com/pub/git.from.bottom.up.pdf"]],
+  "Javascript":[["hoisting","https://scotch.io/tutorials/understanding-hoisting-in-javascript"],
+  				["dom","https://javascript.info/dom-nodes"],
+  				["Javascript the definition guide","http://www.ldos.si/slo/03_Lectures/65_TP/Gradiva/[Java%20Script]%20O'Reilly-%20JavaScript%20The%20Definitive%20Guide.pdf"],
+  				["Javascriptissexy blogs","http://javascriptissexy.com/16-javascript-concepts-you-must-know-well/"]
+],
+  "Internet":[["How Web Browser works","https://blog.logrocket.com/how-browser-rendering-works-behind-the-scenes-6782b0e8fb10"],
+  			["How Internet Works","https://medium.com/@adamzerner/overview-of-computer-networking-70848bd62710"],
+  			["Routing bascis(Think like a computer)","https://www.think-like-a-computer.com/2011/08/24/the-routing-table/"],
+  			["Port Forwarding","https://learn.g2crowd.com/port-forwarding"]]
+};
+
+
+
+for (i=0;i<a.length;i++)
 {
 	var btn = document.createElement("button");
 	btn.classList="collapsible";
@@ -21,26 +42,19 @@ for (i=0;i<3;i++)
 	container.appendChild(btn);
 	var button_div = document.createElement("div");
 	button_div.classList="content";
-	for(k=0;k<b[i].length;k++){
-		var link_button = document.createElement("button");
+	for(k=0;k<links[a[i]].length;k++){
+		link_button = document.createElement("button");
 		link_button.classList= "link_button";
+		let outer_key = a[i];
+		let innner_index = k;
 		link_button.addEventListener("click", function() {
+			console.log(outer_key);
+			console.log(innner_index);
+			window.open(links[outer_key][innner_index][1]);
 		});
-		var t = document.createTextNode(b[i][k][0]);
+		var t = document.createTextNode(links[outer_key][innner_index][0]);
 		link_button.appendChild(t);
 		button_div.appendChild(link_button);
 	}
 	container.appendChild(button_div);
 }
-
-/*for (i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		} 
-	});
-}*/
